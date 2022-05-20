d743bd1df335cf00c568e9260c0d08b7a5faa993
anubhav-pandey1 <73362441+anubhav-pandey1@users.noreply.github.com>
2022-05-20 12:55:19 +0530

    Models and entities v3

--- core/models.py
+++ core/models.py
@@ -39,9 +39,9 @@ class AccessLevel(models.Model):
 
 class Permission(models.Model):
 
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    document_id = models.ForeignKey(Document, on_delete=models.CASCADE)
-    access_level_id = models.ForeignKey(AccessLevel, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    access_level = models.ForeignKey(AccessLevel, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True)
