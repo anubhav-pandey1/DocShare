28876af2f86af73abc7b705e81670b0e61c1f52b
anubhav-pandey1 <73362441+anubhav-pandey1@users.noreply.github.com>
2022-05-20 13:39:52 +0530

    Unauthorized access level

--- core/choices.py
+++ core/choices.py
@@ -1,10 +1,12 @@
 class AccessLevelChoices:
 
+    UNAUTHORIZED = 1
     VIEWER = 10
     EDITOR = 50
     OWNER = 100
 
     LIST = [
+        (UNAUTHORIZED, "Unauthorized"),
         (VIEWER, "Viewer"),
         (EDITOR, "Editor"),
         (OWNER, "Owner"),
