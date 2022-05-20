8f06e9e4136e8a2f2718f47c5d0ac302c41c1e2d
anubhav-pandey1 <73362441+anubhav-pandey1@users.noreply.github.com>
2022-05-20 13:40:20 +0530

    Model serializers v2

--- core/serializers.py
+++ core/serializers.py
@@ -8,6 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
+        fields = '__all__'
 
 
 class DocumentSerializer(serializers.ModelSerializer):
@@ -15,6 +16,7 @@ class DocumentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Document
+        fields = '__all__'
 
 
 class PermissionSerializer(serializers.ModelSerializer):
